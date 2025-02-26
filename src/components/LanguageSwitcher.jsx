0864e89212dea6b1079982c3e4bb8a03a167ff0a
Doğukan Tekin <dogukantekin.ce@gmail.com>
2025-02-26 15:44:43 +0300

    bugfix done (language switcher)

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -1,17 +1,19 @@
+import './languageswitcher.css'
 import { useLanguage } from "../context/LanguageContext";
+
 
 const LanguageSwitcher = () => {
     const { language, toggleLanguage } = useLanguage();
 
     return (
         <div className="language-switcher">
-            <span className={`language-option ${language === "en" ? "disabled" : "active"}`}
+            <span className={`language-option ${language === "en" ? "disabled" : ""}`}
                 onClick={language === "en" ? undefined : toggleLanguage}
             >
                 EN
             </span>
             <span className="separator">|</span>
-            <span className={`language-option ${language === "tr" ? "disabled" : "active"}`}
+            <span className={`language-option ${language === "tr" ? "disabled" : ""}`}
                 onClick={language === "tr" ? undefined : toggleLanguage}
             >
                 TR    
